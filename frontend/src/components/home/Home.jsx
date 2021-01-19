ee9c24be96cdd3fbb8f6547c008c0f13035f76e8
Carlos Severiano <carlossjr@gmail.com>
2021-01-19 20:53:30 -0300

    first commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+    subtitle="Segundo projeto de React">
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Este é um exemplo de cadastro com React!</p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido <i className="fa fa-bolt text-danger"></i> por <strong> Carlos </strong>
+        </span>
+    </footer>
